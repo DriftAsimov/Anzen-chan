6e06d4cc0a7caad703aa11d46ecbe3cad59b8724
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-07 16:09:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import random
 import asyncio
 import datetime
-import jishaku
 import os
 
 client = commands.Bot(command_prefix="::",intents = discord.Intents.all(), case_insensitive = True, help_command=None)
@@ -16,7 +15,6 @@ function_color = 0xE7E874
 async def on_ready():
     print("Let's win this one")
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your IP Address"))
-    client.load_extension('jishaku')
 
 @client.command()
 @commands.is_owner()
@@ -29,4 +27,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("TOKEN")
+client.run("ODEzMzUwODg2Njc1ODQxMDY1.YDOCCg.-LlbJnJQ_uOfiyr6KbWzJw1NRhA")
