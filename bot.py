bc339b01b1a22db4e39bd494c85c3c03b8f0e682
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-07 17:38:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import datetime
 import os
 
 client = commands.Bot(command_prefix="::",intents = discord.Intents.all(), case_insensitive = True, help_command=None)
+TOKEN = os.environ["TOKEN"]
 
 exception_color = 0x90D9CD
 utility_color = 0xD97B88
@@ -15,6 +16,7 @@ function_color = 0xE7E874
 async def on_ready():
     print("Let's win this one")
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your IP Address"))
+    print(TOKEN)
 
 @client.command()
 @commands.is_owner()
@@ -27,4 +29,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(os.environ["TOKEN"])
+client.run(TOKEN)
